f998fe2a81ed13903e2710694bcd673052968bdc
bond <ilkerzaimoglu@gmail.com>
2019-10-16 10:27:18 -0700

    trying to solve function

--- js/app.js
+++ js/app.js
@@ -7,12 +7,17 @@ function Horn(horn) {
   this.description = horn.description
   this.keyword = horn.keyword
   this.horns = horn.horns
+  this.image_url = horn.image_url
+
+  allHorn.push(this);
 }
 
 $.get('../page-1.json'), data => {
   data.forEach(horn => {
-    new Horn (horn).render(); 
-  })
+    let insturement = new Horn(horn)
+    insturement.render(); 
+  });
+  console.log(allHorn);
 }
 
 Horn.prototype.render = function() {
@@ -20,6 +25,10 @@ Horn.prototype.render = function() {
   const $newSection = $('<section></section>');
   $newSection.html(myHornTemp)
 
-  $newSection.find('h2').text(thi)
+  $newSection.find('h2').text(this.title)
+  $newSection.find('img').attr('src', this.image_url)
+  $newSection.find('p').text(this.description)
+
+  $('main').append($newSection);
 }
 
