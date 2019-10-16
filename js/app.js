c7c424e77567a19d2d6c4276007577caf6d06a5d
bond <ilkerzaimoglu@gmail.com>
2019-10-16 09:43:37 -0700

    template done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const allHorn = [] ;
+
+function Horn(horn) {
+  this.title = horn.title
+  this.description = horn.description
+  this.keyword = horn.keyword
+  this.horns = horn.horns
+}
+
+$.get('../page-1.json'), data => {
+  data.forEach(horn => {
+    new Horn (horn).render(); 
+  })
+}
+
+Horn.prototype.render = function() {
+  const myHornTemp = $('#horn-temp').html();
+  const $newSection = $('<section></section>');
+  $newSection.html(myHornTemp)
+}
+
